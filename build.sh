6e9d5147d430497426968202f4a757be9d8e8b69
Mirai Hattori <mirai2045@gmail.com>
2019-12-19 10:31:32 +0900

    remove Dockerfile.intel Dockerfile.nvidia

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-# TODO use $ ubuntu-drivers devices
-if type nvidia-smi; then
-    echo "A nvidia driver environment detected."
-    docker build . -f ./Dockerfile.nvidia -t ibisssl/grsim
-else
-    echo "Non nvidia driver environment detected."
-    docker build . -f ./Dockerfile.intel -t ibisssl/grsim
-fi
-
+docker build . -t ibisssl/grsim
